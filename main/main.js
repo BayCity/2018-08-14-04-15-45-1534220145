60e13ed62c8eaac738836dafb0f71d3ee6739a0e
BayCity <984851056@qq.com>
2019-05-25 23:25:06 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,7 +1,11 @@
 // Write your cade below:
 module.exports = function main(a,b) {
-
-    var c=a % b;
-    console.log(c);
+    //参数判断，是否为数字
+    if (typeof a !== 'number'||typeof a !== 'number') {
+        throw 'Not a number';
+    }
+    var c=a%b;
+ 
     return c;
+    
 };
